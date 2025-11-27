3282b9b5e272c46e176caa22c7f063dec84e9bc9
Manjeet <manjeet@Manjeets-MacBook-Pro.local>
2025-11-27 10:12:50 +0530

    finally solved

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/api/gemini/prompt/send", async (req, res) => {
   }
 
   try {
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.API_KEY}`;
 
     const fetchResponse = await fetch(url, {
       method: "POST",
